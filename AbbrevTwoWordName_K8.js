476e51a5cfb071f88b684bdf4f9282847cb8a140
chollid <chrisuga@me.com>
2021-09-11 17:34:04 -0400

    K8

---
+++ AbbrevTwoWordName_K8.js
@@ -0,0 +1,17 @@
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+// It should look like this:
+
+// Sam Harris => S.H
+
+// Patrick Feeney => P.F
+
+const abbrevName = (name) {
+    let arr = name.split(' ')
+
+    let finalArr = arr.map((item)=> item[0])
+
+    return finalArr.join('.').toUpperCase()
+}
